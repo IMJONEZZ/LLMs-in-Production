e10d8f7416ecbe6d5c8dd8e30ea543f357237f80
Chris Brousseau <chrisbrousseau304@gmail.com>
2023-07-04 15:56:14 -0600

    Adding Listings for Chapter 2 and 4

---
+++ chapters/chapter_4/listing_1.scratchGPT.py
@@ -0,0 +1,189 @@
+import torch
+
+batch_size = 64 # Number of utterances at once
+block_size = 256 # Maximum context window size
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embed = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+
+with open('./shakespeare.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+utt2int = {ch:i for i,ch in enumerate(chars)}
+int2utt = {i:ch for i,ch in enumerate(chars)}
+encode = lambda s: [utt2int[c] for c in s]
+decode = lambda l: ''.join([int2utt[i] for i in l])
+
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    idx = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in idx])
+    y = torch.stack([data[i+1:i+block_size+1] for i in idx])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+class Head(torch.nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = torch.nn.Linear(n_embed, head_size, bias=False)
+        self.query = torch.nn.Linear(n_embed, head_size, bias=False)
+        self.value = torch.nn.Linear(n_embed, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        attention = q @ k.transpose(-2,-1) * k.shape[-1]**0.5
+        attention = attention.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        attention = torch.nn.functional.softmax(attention, dim=-1)
+        attention = self.dropout(attention)
+
+        v = self.value(x)
+        out = attention @ v
+        return out
+    
+class MultiHeadAttention(torch.nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = torch.nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.projection = torch.nn.Linear(head_size * num_heads, n_embed)
+        self.dropout = torch.nn.Dropout(dropout)
+        
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.projection(out))
+        return out
+    
+class FeedFoward(torch.nn.Module):
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(n_embed, 4 * n_embed),
+            torch.nn.ReLU(),
+            torch.nn.Linear(4 * n_embed, n_embed),
+            torch.nn.Dropout(dropout),
+        )
+        
+    def forward(self, x):
+        return self.net(x)
+    
+class Block(torch.nn.Module):
+    def __init__(self, n_embed, n_head):
+        super().__init__()
+        head_size = n_embed // n_head
+        self.self_attention = MultiHeadAttention(n_head, head_size)
+        self.feed_forward = FeedFoward(n_embed)
+        self.ln1 = torch.nn.LayerNorm(n_embed)
+        self.ln2 = torch.nn.LayerNorm(n_embed)
+        
+    def forward(self, x):
+        x = x + self.self_attention(self.ln1(x))
+        x = x + self.feed_forward(self.ln2(x))
+        return x
+    
+class GPT(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding = torch.nn.Embedding(vocab_size, n_embed)
+        self.positional_embedding = torch.nn.Embedding(block_size, n_embed)
+        self.blocks = torch.nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = torch.nn.LayerNorm(n_embed)
+        self.lm_head = torch.nn.Linear(n_embed, vocab_size)
+        
+        self.apply(self._init_weights)
+        
+    def _init_weights(self, module):
+        if isinstance(module, torch.nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, torch.nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            
+    def forward(self, idx, targets=None):
+        B,T = idx.shape
+        
+        tok_emb = self.token_embedding(idx)
+        pos_emb = self.positional_embedding(torch.arange(T, device=device))
+        x = tok_emb + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
+        
+        if targets is None:
+            loss = None
+        else:
+            B,T,C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = torch.nn.functional.cross_entropy(logits, targets)
+            
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]
+            logits, loss = self(idx_cond)
+            logits = logits[:,-1,:]
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+    
+model = GPT()
+m = model.to(device)
+print(sum(p.numel() for p in m.parameters())/1e6, 'Model parameters')
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"| step {iter}: train loss {losses['train']:.4f} | validation loss {losses['val']:.4f} |")
+        
+    xb, yb = get_batch('train')
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+    
+torch.save(model.state_dict(), "./models/scratchGPT/model.pt")
+
+loaded = GPT().load_state_dict("./models/scratchGPT/model.pt")
+    
+context = torch.zeros((1,1), dtype=torch.long, device=device)
+print(decode(loaded.generate(context, max_new_tokens=500)[0].tolist()))
+
+# Save to a file
+#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))
---
+++ chapters/chapter_4/listing_2.betterGPT.py
@@ -0,0 +1,54 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config, DataCollatorForLanguageModeling, TrainingArguments, Trainer
+from datasets import load_dataset
+
+dataset = load_dataset('text', data_files='./shakespeare.txt')
+dataset = dataset.filter(lambda sentence: len(sentence['text']) > 1)
+print(dataset['train'][0])
+
+config = GPT2Config(
+    vocab_size=50261,
+    n_positions=256,
+    n_embd=768,
+    activation_function="gelu",
+)
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+special_tokens_dict = {'bos_token': '<BOS>', 'eos_token': '<EOS>', 'pad_token': '<PAD>', 'mask_token': '<MASK>'}
+tokenizer.add_special_tokens(special_tokens_dict)
+model = GPT2LMHeadModel.from_pretrained("gpt2", config=config, ignore_mismatched_sizes=True)
+
+def tokenize(batch):
+    return tokenizer(str(batch), padding="max_length", truncation=True, max_length=256)
+
+enc_ds = dataset.map(tokenize, batched=False)
+print(f"Tokenized: {enc_ds['train'][0]}")
+
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+)
+
+train_args = TrainingArguments(
+    output_dir="./models/betterGPT/",
+    num_train_epochs=1,
+    per_device_train_batch_size=8,
+    save_steps=5000,
+    save_total_limit=2,
+    report_to="none",
+)
+
+trainer = Trainer(
+    model=model,
+    args=train_args,
+    data_collator=data_collator,
+    train_dataset=enc_ds['train'],
+)
+
+trainer.train()
+trainer.save_model()
+
+model = GPT2LMHeadModel.from_pretrained("./models/betterGPT/")
+
+input = "To be or not"
+tokenized_inputs = tokenizer(input, return_tensors="pt")
+out = model.generate(input_ids=tokenized_inputs['input_ids'], attention_mask=tokenized_inputs['attention_mask'], max_length=256, num_beams=5, temperature=0.7, top_k=50, top_p=0.90, no_repeat_ngram_size=2) 
+print(tokenizer.decode(out[0], skip_special_tokens=True))
