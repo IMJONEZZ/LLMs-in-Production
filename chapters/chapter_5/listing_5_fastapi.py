ba80c389311f4004f73b06c1cb296cc69e967c62
Matt Sharp <matthew.sharp@shopify.com>
2023-07-30 21:25:38 -0600

    Access tokens and rate limiters listing

---
+++ chapters/chapter_5/listing_5_fastapi.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Depends, HTTPException, status, Request
+from fastapi.security import OAuth2PasswordBearer
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+api_keys = ["1234567abcdefg"]  # This would be encrypted in a database
+API_KEY_NAME = "access_token"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+limiter = Limiter(key_func=get_remote_address)
+
+app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
+async def get_api_key(api_key: str = Depends(oauth2_scheme)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid API Key",
+        )
+
+@app.get("/hello", dependencies=[Depends(get_api_key)])
+@limiter.limit("5/minute")
+async def hello(request: Request):
+    return {"message": "Hello World"}
---
+++ chapters/chapter_5/test_ratelimiter_apikey.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+# Url for the API endpoint
+url = "http://localhost:8000/hello"
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer 1234567abcdefg'
+}
+
+bad_headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer password'
+}
+
+# Make a request with the correct API key
+response = requests.get(url, headers=headers)
+assert response.status_code == 200
+print(response.json())
+
+# Make a request with the wrong API key
+response = requests.get(url, headers=bad_headers)
+assert response.status_code == 401
+print(response.json())
+
+# Make multiple requests to test the rate limiter
+for i in range(6):
+    response = requests.get(url, headers=headers)
+    print(f"Request {i+1}: Status code: {response.status_code}, Response: {response.text}")
+    time.sleep(1)  # Sleep for 1 second to make the requests in a spaced out manner
