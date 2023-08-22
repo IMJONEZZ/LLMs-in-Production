1ef0c5d2e1d41f3572223cc935f840a3bcdee7e6
Matt Sharp <matthew.sharp@shopify.com>
2023-08-21 21:04:33 -0600

    Adding listing 2.9

---
+++ chapters/chapter_2/listing_2.9.py
@@ -0,0 +1,13 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+MODEL_NAME = "bigscience/bloom"  # change to bloom-3b to test
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+
+prompt = "Hello world! This is my first time running an LLM!"
+
+input_tokens = tokenizer.encode(prompt, return_tensors="pt", padding=True)
+generated_tokens = model.generate(input_tokens, max_new_tokens=20)
+generated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+print(generated_text)
