2935d0fdb721fdef46681dd7f5de41e1583c2914
Chris Brousseau <chrisbrousseau304@gmail.com>
2024-02-21 21:08:02 -0700

    llama from scratch broken again, but code skeleton there

--- chapters/chapter_11/listing_11.3.langchain_server.py
+++ chapters/chapter_11/listing_11.3.langchain_server.py
@@ -24,12 +24,12 @@ llm = ChatOpenAI(
     n=1,
 )
 
-#Embeddings for RAG
+# Embeddings for RAG
 embedder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-#Tokenization for checking context length quickly
+# Tokenization for checking context length quickly
 tiktoker = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
-#Change the prompt to be whatever you want
+# Change the prompt to be whatever you want
 prompt_template = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
         ###Instruction:
         You are an expert python developer.
@@ -48,46 +48,50 @@ prompt_template = """Below is an instruction that describes a task, paired with 
         ###Response:
         """
 
-#Here's a rudimentary vectorDB, change it to anything we've covered
+# Here's a rudimentary vectorDB, change it to anything we've covered
 vectorDB = load_dataset(
     "csv", data_files="your dataset with embeddings.csv", split="train"
 )
-#If you haven't created a faiss or elasticsearch or usearch index, do it
+# If you haven't created a faiss or elasticsearch or usearch index, do it
 try:
     vectorDB.load_faiss_index("embeddings", "my_index.faiss")
 except:
     print(
         "No faiss index, run vectorDB.add_faiss_index(column='embeddings') and vectorDB.save_faiss_index('embeddings', 'my_index.faiss')"
     )
-    
-#To keep track of chat history, do this however you need!
+
+# To keep track of chat history, do this however you need!
 message_history = []
 
-#Search the Vector DB
+# Search the Vector DB
 query = "How can I train an LLM from scratch?"
 embedded = embedder.encode(query)
 q = np.array(embedded, dtype=np.float32)
 _, retrieved_example = vectorDB.get_nearest_examples("embeddings", q, k=1)
 
-#Format the prompt
+# Format the prompt
 formatted_prompt = PromptTemplate(
     input_variables=["question", "conversation_history", "code_snippet"],
     template=prompt_template,
 )
-#Set up the actual LLM Chain
+# Set up the actual LLM Chain
 chain = LLMChain(llm=llm, prompt=formatted_prompt)
 
-#Don't overload your context length
+# Don't overload your context length
 num_tokens = len(
-    tiktoker.encode(f"{prompt_template},\n" + "\n".join(message_history) + query)
+    tiktoker.encode(
+        f"{prompt_template},\n" + "\n".join(message_history) + query
+    )
 )
 while num_tokens >= 4000:
     message_history.pop(0)
     num_tokens = len(
-        tiktoker.encode(f"{prompt_template},\n" + "\n".join(message_history) + query)
+        tiktoker.encode(
+            f"{prompt_template},\n" + "\n".join(message_history) + query
+        )
     )
 
-#Run RAG with your API
+# Run RAG with your API
 res = chain.run(
     {
         "question": query,
@@ -97,5 +101,5 @@ res = chain.run(
 )
 message_history.append(f"User: {query}\nLlama: {res}")
 
-#I'm just printing, do whatever you need to here
+# I'm just printing, do whatever you need to here
 print(res)
