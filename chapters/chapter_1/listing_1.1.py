155a51d2209f5dbc4db1d229a7ccb6e3eb57b9ed
matt <msharp9@outlook.com>
2024-04-18 21:32:10 -0600

    Update Chapter 1 listings

--- chapters/chapter_1/listing_1.1.py
+++ chapters/chapter_1/listing_1.1.py
@@ -1,10 +1,13 @@
 import os
-import openai
+from openai import OpenAI
 
 # Load your API key from an environment variable
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(
+    # This isn't technically needed as we are passing in the default key
+    api_key=os.getenv("OPENAI_API_KEY")
+)
 
-chat_completion = openai.ChatCompletion.create(
+chat_completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[{"role": "user", "content": "Hello world"}],
 )
