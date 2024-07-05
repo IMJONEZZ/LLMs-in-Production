f188962b74503ef2a53f7197e411c351c6643714
matt <msharp9@outlook.com>
2024-07-05 17:06:55 -0600

    Fixing RLHF, not cleaning because going to redo

--- chapters/appendix_b/RLHF.py
+++ chapters/appendix_b/RLHF.py
@@ -32,6 +32,182 @@ from trl.trainer import ConstantLengthDataset
 from trl.core import LengthSampler
 
 
+def prepare_sample_text(example):
+    "Prepare the text from a sample of the Supervised FineTuning dataset."
+    text = f"Question: {example['question']}\n\n" \
+        f"Answer: {example['response_j']}"
+    return text
+
+
+def chars_token_ratio(dataset, tokenizer, nb_examples=400):
+    """
+    Estimate the average number of characters per token in the Supervised 
+    FineTuning dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(
+        zip(range(nb_examples), iter(dataset)), total=nb_examples
+    ):
+        text = prepare_sample_text(example)
+        total_characters += len(text)
+        if tokenizer.is_fast:
+            total_tokens += len(tokenizer(text).tokens())
+        else:
+            total_tokens += len(tokenizer.tokenize(text))
+
+    return total_characters / total_tokens
+
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the Supervised 
+    FineTuning model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: "
+        f"{all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+
+def create_datasets(tokenizer):
+    # Supervised Finetuning Creation of Datasets
+    dataset = load_dataset(
+        "lvwerra/stack-exchange-paired",
+        data_dir="data/finetune",
+        split="train",
+        use_auth_token=True,
+        num_proc=None,
+        streaming=True,
+    )
+
+    print("Loading the dataset in streaming mode")
+    valid_data = dataset.take(4000)
+    train_data = dataset.skip(4000)
+    train_data = train_data.shuffle(buffer_size=5000, seed=8855)
+
+    chars_per_token = chars_token_ratio(train_data, tokenizer)
+    print(
+        "The character to token ratio of the dataset is: "
+        f"{chars_per_token:.2f}"
+    )
+
+    train_dataset = ConstantLengthDataset(
+        tokenizer,
+        train_data,
+        formatting_func=prepare_sample_text,
+        infinite=True,
+        seq_length=1024,
+        chars_per_token=chars_per_token,
+    )
+    valid_dataset = ConstantLengthDataset(
+        tokenizer,
+        valid_data,
+        formatting_func=prepare_sample_text,
+        infinite=False,
+        seq_length=1024,
+        chars_per_token=chars_per_token,
+    )
+    return train_dataset, valid_dataset
+
+
+def run_training(train_data, val_data):
+    print("Loading the Supervised FineTuning model")
+
+    lora_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    train_data.start_iteration = 0
+
+    print("Starting main Supervised FineTuning loop")
+
+    training_args = TrainingArguments(
+        output_dir="./chapters/appendix_b/models/RLHF",
+        dataloader_drop_last=True,
+        evaluation_strategy="steps",
+        max_steps=5000,
+        eval_steps=1000,
+        save_steps=1000,
+        logging_steps=1,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
+        learning_rate=1e-5,
+        lr_scheduler_type="cosine",
+        warmup_steps=100,
+        gradient_accumulation_steps=1,
+        gradient_checkpointing=True,
+        fp16=False,
+        bf16=False,
+        weight_decay=0.05,
+        run_name="llama2-7b-finetuned",
+        report_to="none",
+        ddp_find_unused_parameters=False,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        "meta-llama/Llama-2-7b-chat-hf",
+        load_in_8bit=True,
+        device_map={"": Accelerator().process_index},
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        peft_config=lora_config,
+        packing=True,
+    )
+
+    print_trainable_parameters(trainer.model)
+
+    print("Supervised FineTuning...")
+    trainer.train()
+
+    print("Saving last checkpoint of the Supervised FineTuning model")
+    trainer.model.save_pretrained(
+        "./chapters/appendix_b/models/RLHF/final_SFT_checkpoint/"
+    )
+
+
+def compute_metrics(eval_pred):
+    # Computing eval metrics for Reward Modeling
+    predictions, _ = eval_pred
+    # Here, predictions is rewards_j and rewards_k.
+    # We want to see how much of the time rewards_j > rewards_k.
+    predictions = np.argmax(predictions, axis=0)
+    labels = np.zeros(predictions.shape)
+    accuracy = evaluate.load("accuracy")
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+class RewardTrainer(Trainer):
+    # Define how to compute the reward loss for Reward Modeling.
+    def compute_loss(self, model, inputs, return_outputs=False):
+        rewards_j = model(
+            input_ids=inputs["input_ids_j"],
+            attention_mask=inputs["attention_mask_j"],
+        )[0]
+        rewards_k = model(
+            input_ids=inputs["input_ids_k"],
+            attention_mask=inputs["attention_mask_k"],
+        )[0]
+        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
+        if return_outputs:
+            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
+        return loss
+
+
 @dataclass
 class RewardDataCollatorWithPadding:
     # Data Collator for Reward Modeling
@@ -81,224 +257,6 @@ class RewardDataCollatorWithPadding:
         return batch
 
 
-class RewardTrainer(Trainer):
-    # Define how to compute the reward loss for Reward Modeling.
-    def compute_loss(self, model, inputs, return_outputs=False):
-        rewards_j = model(
-            input_ids=inputs["input_ids_j"],
-            attention_mask=inputs["attention_mask_j"],
-        )[0]
-        rewards_k = model(
-            input_ids=inputs["input_ids_k"],
-            attention_mask=inputs["attention_mask_k"],
-        )[0]
-        loss = -nn.functional.logsigmoid(rewards_j - rewards_k).mean()
-        if return_outputs:
-            return loss, {"rewards_j": rewards_j, "rewards_k": rewards_k}
-        return loss
-
-
-def chars_token_ratio(dataset, tokenizer, nb_examples=400):
-    """
-    Estimate the average number of characters per token in the Supervised FineTuning dataset.
-    """
-    total_characters, total_tokens = 0, 0
-    for _, example in tqdm(
-        zip(range(nb_examples), iter(dataset)), total=nb_examples
-    ):
-        text = prepare_sample_text(example)
-        total_characters += len(text)
-        if tokenizer.is_fast:
-            total_tokens += len(tokenizer(text).tokens())
-        else:
-            total_tokens += len(tokenizer.tokenize(text))
-
-    return total_characters / total_tokens
-
-
-def print_trainable_parameters(model):
-    """
-    Prints the number of trainable parameters in the Supervised FineTuning model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
-    )
-
-
-def prepare_sample_text(example):
-    "Prepare the text from a sample of the Supervised FineTuning dataset."
-    text = f"Question: {example['question']}\n\nAnswer: {example['response_j']}"
-    return text
-
-
-def create_datasets(tokenizer):
-    # Supervised Finetuning Creation of Datasets
-    dataset = load_dataset(
-        "lvwerra/stack-exchange-paired",
-        data_dir="data/finetune",
-        split="train",
-        use_auth_token=True,
-        num_proc=None,
-        streaming=True,
-    )
-
-    print("Loading the dataset in streaming mode")
-    valid_data = dataset.take(4000)
-    train_data = dataset.skip(4000)
-    train_data = train_data.shuffle(buffer_size=5000, seed=8855)
-
-    chars_per_token = chars_token_ratio(train_data, tokenizer)
-    print(
-        f"The character to token ratio of the dataset is: {chars_per_token:.2f}"
-    )
-
-    train_dataset = ConstantLengthDataset(
-        tokenizer,
-        train_data,
-        formatting_func=prepare_sample_text,
-        infinite=True,
-        seq_length=1024,
-        chars_per_token=chars_per_token,
-    )
-    valid_dataset = ConstantLengthDataset(
-        tokenizer,
-        valid_data,
-        formatting_func=prepare_sample_text,
-        infinite=False,
-        seq_length=1024,
-        chars_per_token=chars_per_token,
-    )
-    return train_dataset, valid_dataset
-
-
-def compute_metrics(eval_pred):
-    # Computing eval metrics for Reward Modeling
-    predictions, _ = eval_pred
-    # Here, predictions is rewards_j and rewards_k.
-    # We want to see how much of the time rewards_j > rewards_k.
-    predictions = np.argmax(predictions, axis=0)
-    labels = np.zeros(predictions.shape)
-    accuracy = evaluate.load("accuracy")
-    return accuracy.compute(predictions=predictions, references=labels)
-
-
-def build_dataset(
-    tokenizer,
-    dataset_name="lvwerra/stack-exchange-paired",
-):
-    """
-    Build RL dataset for training. This builds the dataset from `load_dataset`, one should
-    customize this function to train the model on its own dataset.
-    """
-
-    # load imdb with datasets
-    ds = load_dataset(dataset_name, data_dir="data/rl", split="train")
-    ds = ds.select(range(100000))
-    original_columns = ds.column_names
-    num_proc = 24
-
-    def preprocess_function(examples):
-        new_examples = {
-            "query": [],
-            "input_ids": [],
-        }
-        for question in examples["question"]:
-            query = "Question: " + question + "\n\nAnswer: "
-            tokenized_question = tokenizer(query, truncation=True)
-            new_examples["query"].append(query)
-            new_examples["input_ids"].append(
-                tokenized_question["input_ids"]
-            )
-
-        return new_examples
-
-    ds = ds.map(
-        preprocess_function,
-        batched=True,
-        num_proc=num_proc,
-        remove_columns=original_columns,
-    )
-    ds = ds.filter(lambda x: len(x["input_ids"]) < 512, batched=False)
-
-    ds.set_format(type="torch")
-    return ds
-
-
-def collator(data):
-    # Data Collator for Reinforcement Learning
-    return dict((key, [d[key] for d in data]) for key in data[0])
-
-
-def run_training(train_data, val_data):
-    print("Loading the Supervised FineTuning model")
-
-    lora_config = LoraConfig(
-        r=16,
-        lora_alpha=32,
-        lora_dropout=0.05,
-        bias="none",
-        task_type="CAUSAL_LM",
-    )
-
-    train_data.start_iteration = 0
-
-    print("Starting main Supervised FineTuning loop")
-
-    training_args = TrainingArguments(
-        output_dir="./chapters/chapter_4/models/RLHF",
-        dataloader_drop_last=True,
-        evaluation_strategy="steps",
-        max_steps=5000,
-        eval_steps=1000,
-        save_steps=1000,
-        logging_steps=1,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
-        learning_rate=1e-5,
-        lr_scheduler_type="cosine",
-        warmup_steps=100,
-        gradient_accumulation_steps=1,
-        gradient_checkpointing=True,
-        fp16=False,
-        bf16=False,
-        weight_decay=0.05,
-        run_name="llama2-7b-finetuned",
-        report_to="none",
-        ddp_find_unused_parameters=False,
-    )
-
-    model = AutoModelForCausalLM.from_pretrained(
-        "meta-llama/Llama-2-7b-chat-hf",
-        load_in_8bit=True,
-        device_map={"": Accelerator().process_index},
-    )
-
-    trainer = SFTTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_data,
-        eval_dataset=val_data,
-        peft_config=lora_config,
-        packing=True,
-    )
-
-    print_trainable_parameters(trainer.model)
-
-    print("Supervised FineTuning...")
-    trainer.train()
-
-    print("Saving last checkpoint of the Supervised FineTuning model")
-    trainer.model.save_pretrained(
-        "./chapters/chapter_4/models/RLHF/final_SFT_checkpoint/"
-    )
-
-
 def run_reward_modeling(train_data, val_data):
     # Run the Reward modeling training
     model_name_split = "gpt2"
@@ -307,7 +265,7 @@ def run_reward_modeling(train_data, val_data):
     )
 
     training_args = TrainingArguments(
-        output_dir=f"./chapters/chapter_4/models/{output_name}/",
+        output_dir=f"./chapters/appendix_b/models/{output_name}/",
         learning_rate=2e-5,
         per_device_train_batch_size=4,
         per_device_eval_batch_size=1,
@@ -430,15 +388,65 @@ def run_reward_modeling(train_data, val_data):
 
     print("Saving last checkpoint of the Reward Modeling model")
     model.save_pretrained(
-        f"./chapters/chapter_4/models/RLHF/{output_name}/"
+        f"./chapters/appendix_b/models/RLHF/{output_name}/"
     )
+
+
+def build_dataset_preprocess_function(examples, tokenizer):
+    new_examples = {
+        "query": [],
+        "input_ids": [],
+    }
+    for question in examples["question"]:
+        query = "Question: " + question + "\n\nAnswer: "
+        tokenized_question = tokenizer(query, truncation=True)
+        new_examples["query"].append(query)
+        new_examples["input_ids"].append(
+            tokenized_question["input_ids"]
+        )
+
+    return new_examples
+
+
+def build_dataset(
+    tokenizer,
+    dataset_name="lvwerra/stack-exchange-paired",
+):
+    """
+    Build RL dataset for training. This builds the dataset from 
+    `load_dataset`, one should customize this function to train the model 
+    on their own dataset.
+    """
+
+    # load imdb with datasets
+    ds = load_dataset(dataset_name, data_dir="data/rl", split="train")
+    ds = ds.select(range(100000))
+    original_columns = ds.column_names
+    num_proc = 24
+
+    ds = ds.map(
+        build_dataset_preprocess_function,
+        batched=True,
+        num_proc=num_proc,
+        remove_columns=original_columns,
+        fn_kwargs={'tokenizer': tokenizer},
+    )
+    ds = ds.filter(lambda x: len(x["input_ids"]) < 512, batched=False)
+
+    ds.set_format(type="torch")
+    return ds
+
+
+def collator(data):
+    # Data Collator for Reinforcement Learning
+    return dict((key, [d[key] for d in data]) for key in data[0])
 
 
 def run_RL():
     # Run Reinforcement Learning once the SFT and Reward training are done
     config = PPOConfig(
         steps=20000,
-        model_name="./chapters/chapter_4/models/RLHF/final_SFT_checkpoint/",
+        model_name="./chapters/appendix_b/models/RLHF/final_SFT_checkpoint/",
         learning_rate=1.4e-5,
         batch_size=8,
         mini_batch_size=1,
@@ -455,8 +463,8 @@ def run_RL():
     tokenizer = AutoTokenizer.from_pretrained(
         "meta-llama/Llama-2-7b-chat-hf"
     )
-    # GPT-2 tokenizer has a pad token, but it is not eos_token by default. We need to set it to eos_token.
-    # only for this model.
+    # GPT-2 tokenizer has a pad token, but it is not eos_token by default. 
+    # We need to set it to eos_token. only for this model.
 
     if getattr(tokenizer, "pad_token", None) is None:
         tokenizer.pad_token = tokenizer.eos_token
@@ -492,9 +500,9 @@ def run_RL():
         optimizer=optimizer,
     )
 
-    # We then build the sentiment analysis pipeline, passing the model name and the
-    # sentiment analysis pipeline arguments. Let's also make sure to set the device
-    # to the same device as the PPOTrainer.
+    # We then build the sentiment analysis pipeline, passing the model name 
+    # and the sentiment analysis pipeline arguments. Let's also make sure to
+    # set the device to the same device as the PPOTrainer.
     if ppo_trainer.accelerator.num_processes == 1:
         (
             0 if torch.cuda.is_available() else "cpu"
@@ -502,7 +510,9 @@ def run_RL():
     reward_tokenizer = AutoTokenizer.from_pretrained("gpt2")
     reward_tokenizer.pad_token = reward_tokenizer.eos_token
     reward_model = AutoModelForSequenceClassification.from_pretrained(
-        "./chapters/chapter_4/models/RLHF/gpt2_peft_stack-exchange-paired_rmts__100000_2e-5/"
+        "./chapters/appendix_b/models/RLHF/"
+        "gpt2_peft_stack-exchange-paired_rmts__100000_2e-5/",
+        num_labels=1,
     )
     reward_model.config.pad_token_id = reward_tokenizer.eos_token_id
     sentiment_pipe = pipeline(
@@ -514,9 +524,9 @@ def run_RL():
         return_token_type_ids=False,
     )
 
-    # We then define the arguments to pass to the `generate` function. These arguments
-    # are passed to the `generate` function of the PPOTrainer, which is a wrapper around
-    # the `generate` function of the trained model.
+    # We then define the arguments to pass to the `generate` function. These
+    # arguments are passed to the `generate` function of the PPOTrainer,
+    # which is a wrapper around the `generate` function of the trained model
     generation_kwargs = {
         # "min_length": -1,
         "top_k": 0.0,
@@ -570,38 +580,38 @@ def run_RL():
 
         if epoch % 100 == 0:
             ppo_trainer.save_pretrained(
-                "./chapters/chapter_4/models/RLHF/RLedHFed/"
+                "./chapters/appendix_b/models/RLHF/RLedHFed/"
                 + f"step_{epoch}"
             )
 
 
 def main():
     # Supervised FineTuning
-    # tokenizer = AutoTokenizer.from_pretrained(
-    #     "meta-llama/Llama-2-7b-chat-hf"
-    # )
-    # train_dataset, eval_dataset = create_datasets(tokenizer)
-    # run_training(train_dataset, eval_dataset)
+    tokenizer = AutoTokenizer.from_pretrained(
+        "meta-llama/Llama-2-7b-chat-hf"
+    )
+    train_dataset, eval_dataset = create_datasets(tokenizer)
+    run_training(train_dataset, eval_dataset)
 
-    # del train_dataset, eval_dataset
+    del train_dataset, eval_dataset
 
     # Reward Model Training
-    # train_dataset = load_dataset(
-    #     "lvwerra/stack-exchange-paired",
-    #     data_dir="data/reward",
-    #     split="train",
-    # )
-    # train_dataset = train_dataset.select(range(100000))
-    # eval_dataset = load_dataset(
-    #     "lvwerra/stack-exchange-paired",
-    #     data_dir="data/evaluation",
-    #     split="train",
-    # )
-    # eval_dataset = eval_dataset.select(range(50000))
+    train_dataset = load_dataset(
+        "lvwerra/stack-exchange-paired",
+        data_dir="data/reward",
+        split="train",
+    )
+    train_dataset = train_dataset.select(range(100000))
+    eval_dataset = load_dataset(
+        "lvwerra/stack-exchange-paired",
+        data_dir="data/evaluation",
+        split="train",
+    )
+    eval_dataset = eval_dataset.select(range(50000))
 
-    # run_reward_modeling(train_dataset, eval_dataset)
+    run_reward_modeling(train_dataset, eval_dataset)
 
-    # del train_dataset, eval_dataset
+    del train_dataset, eval_dataset
 
     # Reinforcement Learning Training
     tqdm.pandas()
@@ -614,6 +624,6 @@ def main():
 
 if __name__ == "__main__":
     set_seed(8855)
-    os.makedirs("./chapters/chapter_4/models/RLHF", exist_ok=True)
+    os.makedirs("./chapters/appendix_b/models/RLHF", exist_ok=True)
 
     main()
